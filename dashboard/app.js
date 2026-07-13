891b069c652a396f2108ef2250e3f56fadfbf64a
BudE Builder <actions@github.com>
2026-07-13 19:34:45 +0000

    BudE Genesis Generated Build

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,23 +1,2 @@
-console.log(
-"BudE Command Center Loaded"
-);
-
-
-const buttons =
-document.querySelectorAll("button");
-
-
-buttons.forEach(button=>{
-
-
-button.onclick=function(){
-
-alert(
-button.innerText +
-" system activated"
-);
-
-};
-
-
-});
+console.log('BudE Dashboard Online');
+document.getElementById('status').innerHTML='BudE Engine Online';
