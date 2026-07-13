103b920a09243c294f13861c08751ffc886a8e22
bude404-ops <bude404@gmail.com>
2026-07-13 12:34:38 -0700

    Add button click alerts and update console log

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,2 +1,23 @@
-console.log('BudE Dashboard Online');
-document.getElementById('status').innerHTML='BudE Engine Online';
+console.log(
+"BudE Command Center Loaded"
+);
+
+
+const buttons =
+document.querySelectorAll("button");
+
+
+buttons.forEach(button=>{
+
+
+button.onclick=function(){
+
+alert(
+button.innerText +
+" system activated"
+);
+
+};
+
+
+});
